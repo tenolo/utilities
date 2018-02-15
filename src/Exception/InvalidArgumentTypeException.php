8ebacadff44b9ed3dc45aa735fe9b05981f73cdf
Nikita Loges <nl@tenolo.de>
2018-02-15 14:17:02 +0100

    array support

--- src/Exception/InvalidArgumentTypeException.php
+++ src/Exception/InvalidArgumentTypeException.php
@@ -15,12 +15,16 @@ class InvalidArgumentTypeException extends \RuntimeException
 
     /**
      * @param mixed      $argument
-     * @param int        $neededType
+     * @param mixed      $neededType
      * @param int        $code
      * @param \Exception $previous
      */
     public function __construct($argument, $neededType, $code = 0, \Exception $previous = null)
     {
+        if (is_array($neededType)) {
+            $neededType = implode(', ', $neededType);
+        }
+
         $type = (gettype($argument) == 'object') ? get_class($argument) : gettype($argument);
         $message = 'argument has to be type of ' . $neededType . ', ' . $type . ' given';
 
