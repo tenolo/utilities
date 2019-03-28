5b2afbd2f8bdaeea0559b6775bcb659bdc5ebb6c
Nikita Loges <git@loges.one>
2019-03-28 13:36:20 +0100

    ip range

--- src/Utils/IPUtil.php
+++ src/Utils/IPUtil.php
@@ -4,16 +4,18 @@ namespace Tenolo\Utilities\Utils;
 
 /**
  * Class IP
+ *
  * @package Tenolo\Utilities\Utils
- * @author Nikita Loges
+ * @author  Nikita Loges
  * @company tenolo GbR
- * @date 03.11.2014
+ * @date    03.11.2014
  */
 class IPUtil extends BaseUtil
 {
 
     /**
      * @param $address
+     *
      * @return bool
      */
     public static function validIPAddress($address)
@@ -23,6 +25,7 @@ class IPUtil extends BaseUtil
 
     /**
      * @param $address
+     *
      * @return bool
      */
     public static function validIPv4Address($address)
@@ -32,10 +35,25 @@ class IPUtil extends BaseUtil
 
     /**
      * @param $address
+     *
      * @return bool
      */
     public static function validIPv6Address($address)
     {
         return (bool)filter_var($address, FILTER_VALIDATE_IP, FILTER_FLAG_IPV6);
+    }
+
+    /**
+     * @param string $ip
+     *
+     * @return string
+     */
+    public static function getIpRange($ip)
+    {
+        $ip = ip2long($ip);
+        $ipRangeStart = $ip & 0b11111111111111111111111100000000;
+        $ipRangeEnd = $ip | 0b00000000000000000000000011111111;
+
+        return $ipRangeStart.'-'.$ipRangeEnd;
     }
 } 
